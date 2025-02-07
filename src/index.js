345c7f39986ca44374dcb6f8baf94265e464f85c
sphirebluela <laura.sphireblue@gmail.com>
2025-02-07 15:32:36 -0800

    basic form handling
    added typewriter function

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
+function generatePoem(event) {
+  event.preventDefault();
 
+  new Typewriter("#poem", {
+    strings: "Hello World",
+    autoStart: true,
+    cursor: "",
+  });
+}
+
+let poemGeneratorElement = document.querySelector("#poem-generator");
+poemGeneratorElement.addEventListener("submit", generatePoem);
