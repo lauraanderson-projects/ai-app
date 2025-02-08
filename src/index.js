5506269abacbeeb859463ace4f9b48a2116eac17
sphirebluela <laura.sphireblue@gmail.com>
2025-02-07 21:24:38 -0800

    updating user experience
    UX Design

--- src/index.js
+++ src/index.js
@@ -14,10 +14,14 @@ function generatePoem(event) {
 
   let apiKey = "bd809658a5b50o74b7f3fe9fa5dft8a8";
   let context =
-    "you are a writer who loves writing poems. your commision is to generate 5 line poem. each line of the poem should start on a new line using html format. example: Beneath the waves where secrets sigh,<br>The ocean breathes a lullaby,<br>With whispers of a distant shore.";
+    "you are a writer who loves writing poems. your commision is to generate 5 line poem. each line of the poem should start on a new line using html format. example: Beneath the waves where secrets sigh,<br>The ocean breathes a lullaby,<br>With whispers of a distant shore. add a signature at the end with the topic entered.";
   let prompt = `Generate a poem based on the subject ${poemSubject.value}`;
 
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = "Generating Poem...";
 
   axios.get(apiUrl).then(displayPoem);
 }
